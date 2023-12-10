00d7a8e5759712dd882659ceef31b7b7ea251beb
Jan Kondratowicz <jkondratowicz@gmail.com>
2023-12-10 16:18:19 +0100

    better markdown rendering

--- frontend/src/components/NiceMarkdown.tsx
+++ frontend/src/components/NiceMarkdown.tsx
@@ -1,4 +1,4 @@
-import { ListItem, OrderedList, UnorderedList } from '@chakra-ui/react';
+import { ListItem, OrderedList, UnorderedList, Text, Heading } from '@chakra-ui/react';
 import Markdown from 'react-markdown';
 
 export const NiceMarkdown = ({ text }: { text: string }) => {
@@ -16,6 +16,34 @@ export const NiceMarkdown = ({ text }: { text: string }) => {
         li(props) {
           const { node, ...rest } = props;
           return <ListItem {...rest} />;
+        },
+        p(props) {
+          const { node, ...rest } = props;
+          return <Text my={3} {...rest} />;
+        },
+        h1(props) {
+          const { node, ...rest } = props;
+          return <Heading my={3} fontSize="3xl" {...rest} />;
+        },
+        h2(props) {
+          const { node, ...rest } = props;
+          return <Heading my={3} fontSize="2xl" {...rest} />;
+        },
+        h3(props) {
+          const { node, ...rest } = props;
+          return <Heading my={3} fontSize="xl" {...rest} />;
+        },
+        h4(props) {
+          const { node, ...rest } = props;
+          return <Heading my={3} fontSize="lg" {...rest} />;
+        },
+        h5(props) {
+          const { node, ...rest } = props;
+          return <Heading my={3} fontSize="md" {...rest} />;
+        },
+        h6(props) {
+          const { node, ...rest } = props;
+          return <Heading my={3} fontSize="sm" {...rest} />;
         },
       }}
     >
